16da780c465c427a8132645b90b4ea4a5021f99a
LykovaKateryna <k.lykova.ip15@kpi.ua>
2021-11-04 00:29:45 +0200

    Update lab3_1.cpp

--- lab3/cpp/lab3_1.cpp
+++ lab3/cpp/lab3_1.cpp
@@ -4,21 +4,31 @@
 int main()
 {
 	int x;
-	int n;
+	long long n;
 	long double f;
-	long double s=0;
-	double eps=0.0001;
-	unsigned long double factk;
-	unsigned long double factkn;
-	int k = 0;
+	long double s = 0;
+	double eps = 0.0001;
+	long double factk = 1;
+	long double factkn = 1;
+	long long k = 0;
 	std::cout << "Enter x: ";
 	std::cin >> x;
 	std::cout << "Enter n: ";
 	std::cin >> n;
-	if (n == 0)
+	if (n < 0)
 	{
-		factk = 1;
-		factkn = 1;
+		std::cout << "Error " << std::endl;
+	}
+	else if (n > 4)
+	{
+		std::cout << "Error " << std::endl;
+	}
+	else
+	{
+		for (n = 1; n <= 4; n++) 
+		{
+			factkn = factkn * n;
+		}
 		do
 		{
 			f = pow(-1, k) * pow((x / 2), (n + 2 * k)) / (factk * factkn);
@@ -28,79 +38,6 @@ int main()
 			factkn = factkn * (k + n);
 		} while (abs(f) >= eps);
 		std::cout << s << std::endl;
-	}
-	else
-	{
-		if (n == 1)
-		{
-			factk = 1;
-			factkn = 1;
-			do
-			{
-				f = pow(-1, k) * pow((x / 2), (n + 2 * k)) / (factk * factkn);
-				s = s + f;
-				k = k + 1;
-				factk = factk * k;
-				factkn = factkn * (k + n);
-			} while (abs(f) >= eps);
-			std::cout << s << std::endl;
-		}
-		else
-		{
-			if (n == 2)
-			{
-				factk = 1;
-				factkn = 2;
-				do
-				{
-					f = pow(-1, k) * pow((x / 2), (n + 2 * k)) / (factk * factkn);
-					s = s + f;
-					k = k + 1;
-					factk = factk * k;
-					factkn = factkn * (k + n);
-				} while (abs(f) >= eps);
-				std::cout << s << std::endl;
-			}
-			else
-			{
-				if (n == 3)
-				{
-					factk = 1;
-					factkn = 6;	
-					do
-					{
-						f = pow(-1, k) * pow((x / 2), (n + 2 * k)) / (factk * factkn);
-						s = s + f;
-						k = k + 1;
-						factk = factk * k;
-						factkn = factkn * (k + n);
-					} while (abs(f) >= eps);
-					std::cout << s << std::endl;
-				}
-				else
-				{
-					if (n == 4)
-					{
-						factk = 1;
-						factkn = 24;
-						do
-						{
-							f = pow(-1, k) * pow((x / 2), (n + 2 * k)) / (factk * factkn);
-							s = s + f;
-							k = k + 1;
-							factk = factk * k;
-							factkn = factkn * (k + n);
-						} while (abs(f) >= eps);
-						std::cout << s << std::endl;
-					}
-					else
-					{
-						std::cout << "Error " << std::endl;
-					}
-
-				}
-			}
-		}
 
 	}
 	system("pause");
